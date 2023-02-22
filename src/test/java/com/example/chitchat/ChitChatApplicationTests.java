16104ccf5526a4b88028df0bdbe114a22b0b0b5d
Jim <jgmedr0@gmail.com>
2023-02-22 15:44:09 -0600

    feat:FirstCommit

---
+++ src/test/java/com/example/chitchat/ChitChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
