5e6ad2025c85d582aaf5a4ac5cd2045e329a459e
Jim <jgmedr0@gmail.com>
2023-02-22 16:24:08 -0600

    feat::secondcommit

---
+++ src/test/java/com/example/chitchat/config/MongoConnectionTest.java
@@ -0,0 +1,11 @@
+package com.example.chitchat.config;
+
+public class MongoConnectionTest {
+    private static final String HOST = "localhost";
+    private static final String PORT = "27017";
+    private static final String DB = "mongo";
+    private static final String USER = "admin";
+    private static final String PASS = "password";
+
+
+}
